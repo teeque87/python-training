1fca8f5edf63c372a5c977e979fca33ddbe8d52d
Teeque87 <teeque87@gmail.com>
2024-09-09 14:56:39 +0200

    solution found

--- 5_4_Kombinationen/Aufgabe_54_8.py
+++ 5_4_Kombinationen/Aufgabe_54_8.py
@@ -3,3 +3,31 @@
 # stehen gelassen werden und alle Buchstaben dazwischen jeweils wie Jasskarten gemischt werden. 
 # Das heisst, dass keine Buchstaben dazu kommen und auch keine entfernt werden, jedoch die Reihenfolge verändert wird. 
 # Gib anschliessend den so entstandenen Text aus. Was stellst du fest?
+from random import shuffle
+
+input_string = str(input('Gib einen oder mehrere Sätze ein: '))
+input_string = input_string.split()
+new_string = ''
+
+for word in input_string:
+    if len(word) > 3:
+        char_left, char_right = None, None
+        scramble = None
+        length = len(word)
+        if word[0].isalpha() and word[-1].isalpha():
+            char_left = word[0]
+            char_right = word[-1]
+            scramble = list(word[1:-1])
+        if word[0].isalpha() and not word[-1].isalpha():
+            char_left = word[0]
+            char_right = word[-2] + word[-1]
+            scramble = list(word[1:-2])
+        shuffle(scramble)
+        middle = ''.join(scramble)
+        new_word = char_left + middle + char_right
+        new_string += new_word + ' '
+    else:
+        new_string += word + ' '
+
+print(new_string)
+
