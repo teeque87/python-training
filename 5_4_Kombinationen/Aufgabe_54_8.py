47d41836b1f2cc6e3b9c3c5977557912d1412ef4
Teeque87 <teeque87@gmail.com>
2024-09-09 12:36:07 +0200

    init

---
+++ 5_4_Kombinationen/Aufgabe_54_8.py
@@ -0,0 +1,5 @@
+# Schreibe ein Programm, welches den Benutzer nach einem oder mehreren deutschen Sätzen fragt. 
+# Anschliessend verändert das Programm den Text so, dass bei jedem Wort der erste und der letzte Buchstabe 
+# stehen gelassen werden und alle Buchstaben dazwischen jeweils wie Jasskarten gemischt werden. 
+# Das heisst, dass keine Buchstaben dazu kommen und auch keine entfernt werden, jedoch die Reihenfolge verändert wird. 
+# Gib anschliessend den so entstandenen Text aus. Was stellst du fest?
